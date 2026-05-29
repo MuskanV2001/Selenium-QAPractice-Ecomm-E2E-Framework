a4af0ec6b17c4a267a15b85eb3851472e0c18854
Muskan Vishwakarma <muskanv01lko@gmail.com>
2026-05-29 15:04:50 +0530

    Added Cucumber Reports for Jenkins

--- src/test/java/tests/TestNGTestRunner.java
+++ src/test/java/tests/TestNGTestRunner.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Listeners;
         glue = {"stepDefinitions", "src/main/base", "hooks"},
         tags = "@E2E",
         monochrome = true,
-        plugin = {}
+        plugin = {"pretty", "json:target/cucumberReports/cucumber.json"}
 )
 @Listeners(listeners.TestNGListener.class)
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
